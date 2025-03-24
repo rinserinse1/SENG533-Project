a2fcbf838492cfb5d915b3803a259ff48815d25d
rinserinse1 <101231962+rinserinse1@users.noreply.github.com>
2025-03-24 17:39:36 -0600

    summary is generated

--- server/tests/k6_atlas/test.js
+++ server/tests/k6_atlas/test.js
@@ -33,3 +33,21 @@ export default function () {
   // Simulate some wait time between requests
   sleep(1);
 }
+
+
+export function handleSummary(data) {
+    // data contains all end-of-test metrics
+    // We can return an object mapping file names to their contents
+  
+    return {
+      // Save a JSON with only the summary data
+      'summary.json': JSON.stringify(data, null, 2),
+  
+      // Also produce a nice human-readable summary on the console
+      stdout: textSummary(data, { indent: ' ', enableColors: true }),
+    };
+  }
+  
+  // We need this import at the bottom if we want the "textSummary" helper
+  import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+  
